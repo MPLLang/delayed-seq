bfc7b5497e7f7bc8dfd74350b3f27049b7055ccf
Sam Westrick <shwestrick@gmail.com>
2020-11-08 14:27:47 -0500

    make cpp compile into bin/ and also output warmup time

--- cpp/benchmark.hpp
+++ cpp/benchmark.hpp
@@ -12,27 +12,35 @@ namespace pbbsBench {
 
 static
 void cilk_set_nb_workers(int nb_workers) {
-#if defined(PARLAY_CILK)                  
+#if defined(PARLAY_CILK)
   int cilk_failed = __cilkrts_set_param("nworkers", std::to_string(nb_workers).c_str());
   if (cilk_failed) {
     printf("failed\n");
   }
 #endif
 }
-  
+
 void setProc(int nb_proc) {
 #ifndef HOMEGROWN
   cilk_set_nb_workers(nb_proc);
 #endif
 }
-  
+
 void warmup(int nb_proc) {
+  struct timezone tzp({0,0});
+  auto get_time = [&] {
+    timeval now;
+    gettimeofday(&now, &tzp);
+    return ((double) now.tv_sec) + ((double) now.tv_usec)/1000000.;
+  };
   size_t dflt_warmup_n = (nb_proc == 1) ? 5 : 35;
   size_t warmup_n = deepsea::cmdline::parse_or_default_int("warmup", dflt_warmup_n);
+  auto st = get_time();
   for (int i = 0; i < warmup_n; i++) {
     size_t n = 100000000;
     auto A = parlay::tabulate(n, [] (size_t i) -> double {return 1.0;});
   }
+  printf ("warmuptime %.3lf\n", get_time() - st);
 }
-    
+
 } // end namespace
