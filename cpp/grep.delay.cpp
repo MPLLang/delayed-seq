eacb882c53dd4785c68cedaa03da71752d1311ea
Guy Blelloch <guyb@cs.cmu.edu>
2020-11-09 19:05:04 -0500

    changes

--- cpp/grep.delay.cpp
+++ cpp/grep.delay.cpp
@@ -8,14 +8,17 @@
 #include "cppdelayed/parlay/utilities.h"
 #include "cppdelayed/parlay/io.h"
 
-auto grep(parlay::range<char*> str, parlay::sequence<char>& search_str) -> parlay::sequence<char> {
+auto grep(parlay::sequence<char> const &str,
+	  parlay::sequence<char> const &search_str) -> parlay::sequence<char> {
   auto is_even = [] (size_t i) { return i & 1; };
   auto is_line_break = [&] (char a) {return a == '\n';};
-  auto cr = singleton('\n');
-  auto lines = parlay::filter(parlay::split_range(str, is_line_break), [&] (auto const &s) {
-      return parlay::search(s, search_str) < s.size();});
+  auto identity = [] (auto x) {return x;};
+  auto cr = parlay::sequence<char>('\n',1);
+  auto lines = parlay::filter(parlay::map_tokens(str, identity, is_line_break),
+			      [&] (auto const &s) {
+      return parlay::search(s, search_str) < s.end();});
   return parlay::flatten(parlay::tabulate(lines.size()*2, [&] (size_t i) {
-      return is_even(i) ? cr : lines[i/2];}));
+    return is_even(i) ? cr : parlay::to_sequence(lines[i/2]);}));
 }
 
 using namespace std;
@@ -25,10 +28,9 @@ int main(int argc, char** argv) {
   auto pattern_str = deepsea::cmdline::parse_or_default_string("pattern", "xxy");
   auto pattern = parlay::tabulate(pattern_str.size(), [&] (size_t i) { return pattern_str[i]; });
   auto input = parlay::chars_from_file(infile.c_str(), true);
-  auto str = parlay::make_range(input.begin(), input.end());
   parlay::sequence<char> out_str;
   pbbsBench::launch([&] {
-    out_str = grep(str, pattern);
+    out_str = grep(input, pattern);
   });
   std::cout << "result " << out_str.size() << std::endl;
   return 0;
