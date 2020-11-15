66523ff9af62ea5f4b585a3c45bd49f8f122b9ea
mikerainey <bionic.oldsmobile@gmail.com>
2020-11-14 21:16:59 -0500

    adding parens

---
+++ cpp/parens.array.cpp
@@ -0,0 +1,14 @@
+#include "cmdline.hpp"
+#include "benchmark.hpp"
+#include "cppdelayed/parens.h"
+
+int main(int argc, char** argv) {
+  size_t n = std::max((size_t)1, (size_t)deepsea::cmdline::parse_or_default_long("n", 100000000));
+  parlay::sequence<char> str(n, 1);
+  pbbsBench::launch([&] {
+    paren_match_strict(str);
+  });
+  //  cout << "result " << result << endl;
+  return 0;
+}
+
