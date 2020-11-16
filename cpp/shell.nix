cc8d7a1ede1906774c49d69aa279185d4bbcb93a
mikerainey <bionic.oldsmobile@gmail.com>
2020-11-15 20:52:00 -0500

    switching to parlay homegrown scheduler

--- cpp/shell.nix
+++ cpp/shell.nix
@@ -1,8 +1,15 @@
+{ pkgs   ? import <nixpkgs> {},
+  stdenv ? pkgs.stdenv,
+  jemalloc ? pkgs.jemalloc
+#  jemalloc ? pkgs.jemalloc450
+}:
+
 with import <nixpkgs> {}; {
   qpidEnv = stdenvNoCC.mkDerivation {
     name = "my-gcc7-environment";
     buildInputs = [
-        gcc7
+      gcc
+      jemalloc
     ];
   };
 }
